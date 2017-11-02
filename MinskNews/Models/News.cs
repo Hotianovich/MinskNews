8437427cea70e7a715f630edf57795cb300e1327
jack8n <alexhotianovich@gmail.com>
2017-11-02 19:44:12 +0200

    Add model News

---
+++ MinskNews/Models/News.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinskNews.Models
+{
+    public class News
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Display(Name = "Заголовок")]
+        [Required(ErrorMessage = "Введите заголовок")]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'.'MM'.'yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Введите дату")]
+        public DateTime AddDate { get; set; }
+
+        [MaxLength(1000)]
+        [Display(Name = "Краткое описание")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "Полное описание")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string FullText { get; set; }
+
+        [ScaffoldColumn(false)]
+        public byte[] Inage { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string MineType { get; set; }
+
+    }
+}
