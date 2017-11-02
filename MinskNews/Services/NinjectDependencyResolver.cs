f17d138952abea4b71fe0bafd567a43e11e6d99e
jack8n <alexhotianovich@gmail.com>
2017-11-03 00:24:24 +0200

    Add ninject

---
+++ MinskNews/Services/NinjectDependencyResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace MinskNews.Services
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        IKernel kernel;
+        public NinjectDependencyResolver(IKernel krnl)
+        {
+            kernel = krnl;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type servicesType)
+        {
+            return kernel.GetAll(servicesType);
+        }
+    }
+}
