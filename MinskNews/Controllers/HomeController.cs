47349e27a2a8f295820a7e062c11264108de386c
jack8n <alexhotianovich@gmail.com>
2017-11-04 13:58:46 +0200

    Add three news

--- MinskNews/Controllers/HomeController.cs
+++ MinskNews/Controllers/HomeController.cs
@@ -26,6 +26,25 @@ namespace MinskNews.Controllers
             return View(model);
         }
 
+        public PartialViewResult ThreeNews()
+        {
+            var randnews = RandThree();
+            return PartialView(randnews);
+        }
+        public IEnumerable<News> RandThree()
+        {
+            var count = _repoNews.GetAll();
+            Random rand = new Random();
+            List<News> listRand = new List<News>();
+            for (int i = 0; i < 3; i++)
+            {
+                var id = rand.Next(1, count.Count()-1);
+                News news = _repoNews.Get(id);
+                listRand.Add(news);
+            }
+            return listRand;
+        }
+
        
     }
 }
