13774a53916f8a11cf16abba5ba494b05080ca88
jack8n <alexhotianovich@gmail.com>
2017-11-05 11:13:53 +0200

    Update tests

--- NewsTest/UnitTest1.cs
+++ NewsTest/UnitTest1.cs
@@ -17,89 +17,94 @@ namespace NewsTest
         /// <summary>
         /// Проверка разбиения на страницы
         /// </summary>
-        //[TestMethod]
-        //public void PagingTest()
-        //{
-        //    // Arrange
-        //    // Макет репозитория
-        //    var mock = new Mock<IRepository<News>>();
-        //    mock.Setup(r => r.GetAll()).Returns(new List<News> {
-        //                                            new News { Id = 1 },
-        //                                            new News { Id = 2 },
-        //                                            new News { Id = 3 },
-        //                                            new News { Id = 4 },
-        //                                            new News { Id = 5 },
-        //                                            new News { Id = 6 },
-        //                                            new News { Id = 7 }
-        //                                            });
-        //    // Создание объекта контроллера
-        //    var controller = new HomeController(mock.Object);
+        [TestMethod]
+        public void PagingTest()
+        {
+            // Arrange
+            // Макет репозитория
+            var mock = new Mock<IRepository<News>>();
+            mock.Setup(r => r.GetAll()).Returns(new List<News> {
+                                                    new News { Id = 1 },
+                                                    new News { Id = 2 },
+                                                    new News { Id = 3 },
+                                                    new News { Id = 4 },
+                                                    new News { Id = 5 },
+                                                    new News { Id = 6 },
+                                                    new News { Id = 7 }
+                                                    });
+            // Создание объекта контроллера
+            var controller = new HomeController(mock.Object);
 
-        //    // Макеты для получения HttpContext HttpRequest
-        //    var request = new Mock<HttpRequestBase>();
-        //    var httpContext = new Mock<HttpContextBase>();
-        //    httpContext.Setup(h => h.Request).Returns(request.Object);
+            // Макеты для получения HttpContext HttpRequest
+            var request = new Mock<HttpRequestBase>();
+            var httpContext = new Mock<HttpContextBase>();
+            httpContext.Setup(h => h.Request).Returns(request.Object);
 
-        //    // Создание контекста контроллера
-        //    controller.ControllerContext = new ControllerContext();
-        //    controller.ControllerContext.HttpContext = httpContext.Object;
+            // Создание контекста контроллера
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = httpContext.Object;
 
-        //    // Act
-        //    // Вызов метода Index
-        //    var view = controller.Index(2) as ViewResult;
+            // Act
+            // Вызов метода Index
+            var view = controller.Index(2) as ViewResult;
 
 
-        //    // Assert
-        //    Assert.IsNotNull(view, "Представление не получено");
-        //    Assert.IsNotNull(view.Model, "Модель не получена");
-        //    PageListViewModel<News> model = view.Model as PageListViewModel<News>;
-        //    Assert.AreEqual(2, model.Count);
-        //    Assert.AreEqual(6, model[0].Id);
-        //    Assert.AreEqual(7, model[1].Id);
-        //}
+            // Assert
+            Assert.IsNotNull(view, "Представление не получено");
+            Assert.IsNotNull(view.Model, "Модель не получена");
+            PageListViewModel<News> model = view.Model as PageListViewModel<News>;
+            
+            //длинна списка обьектов = 2
+            Assert.AreEqual(2, model.Count);
+            //Id объектов списка соответствуют двум последним элементам списка
+            Assert.AreEqual(6, model[0].Id);
+            Assert.AreEqual(7, model[1].Id);
+        }
 
 
         /// <summary>
         /// Проверка на выборку 3 случайных новостей
         /// </summary>
-        //[TestMethod]
-        //public void RandThreeTest()
-        //{
-        //    // Arrange
-        //    var mock = new Mock<IRepository<News>>();
-        //    mock.Setup(r => r.GetAll()).Returns(new List<News> { new News { Id = 1 } });
+        [TestMethod]
+        public void RandThreeTest()
+        {
+            // Arrange
+            var mock = new Mock<IRepository<News>>();
+            mock.Setup(r => r.GetAll()).Returns(new List<News> { new News { Id = 1 } });
 
-        //    var controller = new HomeController(mock.Object);
+            var controller = new HomeController(mock.Object);
 
-        //    // Act
-        //    // Вызов метода RandThree
-        //    var list = controller.RandThree() as List<News>;
+            // Act
+            // Вызов метода RandThree
+            var list = controller.RandThree() as List<News>;
 
-        //    // Assert
-        //    Assert.IsNotNull(list, "Список не получен");
-        //    Assert.AreEqual(3, list.Count);
-        //}
+            // Assert
+            Assert.IsNotNull(list, "Список не получен");
+            //количество возвращаемых элементов списка  = 3
+            Assert.AreEqual(3, list.Count);
+        }
 
         /// <summary>
         /// Тест метода Create на вызов представления Create в случае ошибки модели
         /// </summary>
-        //[TestMethod]
-        //public void CreatePostAction_ModelError()
-        //{
-        //    // Arrange
-        //    string expected = "Create";
-        //    var mock = new Mock<IRepository<News>>();
-        //    News news = new News();
-        //    var controller = new AdminController(mock.Object);
-        //    controller.ModelState.AddModelError("Name", "Название модели не установлено");
+        [TestMethod]
+        public void CreatePostAction_ModelError()
+        {
+            // Arrange
+            string expected = "Create";
+            var mock = new Mock<IRepository<News>>();
+            News news = new News();
+            var controller = new AdminController(mock.Object);
+            controller.ModelState.AddModelError("Name", "Название модели не установлено");
 
-        //    // Act
-        //    var result = controller.Create(news) as ViewResult;
+            // Act
+            var result = controller.Create(news) as ViewResult;
 
-        //    // Assert
-        //    Assert.IsNotNull(result, "Список не получен");
-        //    Assert.AreEqual(expected, result.ViewName);
-        //}
+            // Assert
+            Assert.IsNotNull(result, "Список не получен");
+            //сравниваем имена вьюшек
+            Assert.AreEqual(expected, result.ViewName);
+        }
 
         /// <summary>
         /// Тест метода Create на переадресацию в представление Index
@@ -113,13 +118,12 @@ namespace NewsTest
             News news = new News();
             var controller = new AdminController(mock.Object);
 
-
             // Act
-            // Вызов метода RandThree
-            RedirectToRouteResult result = controller.Create(news) as RedirectToRouteResult;
+            var result = controller.Create(news) as RedirectToRouteResult;
 
             // Assert
             Assert.IsNotNull(result, "Список не получен");
+            //сравниваем имена методов действия
             Assert.AreEqual(expected, result.RouteValues["action"]);
         }
 
@@ -134,10 +138,8 @@ namespace NewsTest
             News news = new News();
             var controller = new AdminController(mock.Object);
 
-
             // Act
-            // Вызов метода RandThree
-            RedirectToRouteResult result = controller.Create(news) as RedirectToRouteResult;
+            var result = controller.Create(news) as RedirectToRouteResult;
 
             // Assert
             mock.Verify(v => v.Create(news));
